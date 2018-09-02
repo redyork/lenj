98248a3040db385e216a7b31cfb8973cea6dccb0
root <y-ork@mail.ru>
2018-09-02 20:18:30 +0200

    First launch

--- 01_init.sh
+++ 01_init.sh
@@ -2,11 +2,23 @@
 
 source vars
 
+backway=`pwd`
+
 docker pull certbot/certbot
 
-sudo mkdir -p  $lenj_destdir/letsencrypt-site
-cp docker-compose.yml $lenj_destdir/
-cp nginx.conf $lenj_destdir/
-cp index.html $lenj_destdir/letsencrypt-site
+mkdir -p  $lenj_dockerdir/www
+mkdir -p  $lenj_dockerdir/data/nginx
 
+mkdir -p $lenj_dockerdir/data/nginx
 
+openssl dhparam -out $lenj_dockerdir/nginx/dhparam-2048.pem 2048
+
+cp docker-compose.yml $lenj_dockerdir/
+cp nginx.conf $lenj_dockerdir/data/nginx/
+cp index.html $lenj_dockerdir/www/
+
+cd $lenj_dockerdir
+
+docker-compose up -d
+
+cd $backway
--- 02_certbot_test.sh
+++ 02_certbot_test.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 source vars
-sudo docker run --exec-driver=lxc -it --rm -v /docker-volumes/etc/letsencrypt:/etc/letsencrypt -v /docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt \
--v /docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-site:/data/letsencrypt \
--v "/docker-volumes/var/log/letsencrypt:/var/log/letsencrypt" \
+
+docker run -it --rm -v $lenj_dockerdir/data/etc/letsencrypt:/etc/letsencrypt \
+-v $lenj_dockerdir/data/var/lib/letsencrypt:/var/lib/letsencrypt \
+-v $lenj_dockerdir/www:/data/letsencrypt \
+-v $lenj_dockerdir/data/var/log/letsencrypt:/var/log/letsencrypt \
 certbot/certbot \
 certonly --webroot \
 --email $lenj_email --agree-tos --no-eff-email \
 --webroot-path=/data/letsencrypt \
 --staging \
 -d $lenj_domain
+
