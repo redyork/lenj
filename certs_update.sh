54a7cac430374116ddf325faf927d6f859a3f960
root <y-ork@mail.ru>
2018-09-03 09:42:41 +0200

    Done: cron script

--- certs_update.sh
+++ certs_update.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-cd 'PLACE CORRECT PATH TO THIS SCRIPT HERE'
+#cd 'PLACE CORRECT PATH TO THIS SCRIPT HERE'
+
 source vars
 
 docker run -it --rm --name certbot -v $lenj_dockerdir/data/etc/letsencrypt:/etc/letsencrypt \
 -v $lenj_dockerdir/data/var/lib/letsencrypt:/var/lib/letsencrypt \
 -v $lenj_dockerdir/www:/data/letsencrypt \
 -v $lenj_dockerdir/data/var/log/letsencrypt:/var/log/letsencrypt \
-certbot/certbot renew --webroot -w /data/letsencrypt --quiet && docker kill --signal=HUP production-nginx-container
+certbot/certbot renew --webroot -w /data/letsencrypt --quiet
+
+error=""
+for container in $(docker ps | grep 'yorkred/jenkins' | awk '{print $1}'); do
+    echo "------------------------------------------------------------"
+       docker exec $container sudo /usr/sbin/nginx -s reload
+       error=$error$?
+done
+if [ "$error" -gt "0" ]; then
+error=1
+else
+error=0
+fi
+exit $error
